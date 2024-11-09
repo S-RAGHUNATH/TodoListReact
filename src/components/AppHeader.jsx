c913a9ad5b47888e8c4451247358f88a3bea1930
S-RAGHUNATH <74005080+S-RAGHUNATH@users.noreply.github.com>
2024-11-09 19:51:33 +0530

    Update AppHeader.jsx
    adding cmnts on AppHeader.jsx

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -6,7 +6,7 @@ import { NewListDialog } from './NewListDialog.jsx';
 
 export function AppHeader() {
   //PopupState - Takes care of the boilerplate for common Menu, Popover and Popper use cases. Provides a Custom React Hook that keeps track of the local state for a single popup, and functions to connect trigger, toggle, and popover/menu/popper components to the state
-  const dialogState = usePopupState({ variant: 'dialog', popupId: 'new-list' }); //variant: 'dialog', popupId: 'new-list' - one of those kinds
+  const dialogState = usePopupState({ variant: 'dialog', popupId: 'new-list' }); // Initializes the popup state. You specify a variant (like 'dialog') and a unique popupId.
 
   return (
     <>
@@ -24,7 +24,7 @@ export function AppHeader() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            onClick={dialogState.open}
+            onClick={dialogState.open} //.open, .close are methods of usePopState from MUI
           >
             <Add />
           </IconButton>
